a7166c0ae09ef756f51d0d8dd71c6798ee459b1f
RecoveringChef <54086786+RecoveringChef@users.noreply.github.com>
2019-10-28 23:20:18 -0400

    file structure and import/export set up. ver minor css stling. no cards yet and only starter functions

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => (
+    <div className="banner text-center d-flex align-items-center justify-content-center">
+        <p className="m-0">One click, one point! Click the same one again and you're done.</p>
+    </div>
+);
+export default Banner;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => <div className="footer text-center">Dan Crenshaw</div>;
+
+export default Footer;
