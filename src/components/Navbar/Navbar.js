af4fbae064cd0b6a4fda40bff3cf6d9e4583d928
RecoveringChef <54086786+RecoveringChef@users.noreply.github.com>
2019-10-31 00:34:40 -0400

    tweaked top bar for styling and spacing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,11 +3,8 @@ import './Navbar.css';
 
 const Navbar = props => (
     <div className="navbar ">
-        <div className="title">Puppies 'n Kittens</div>
-        <div className={props.navMsgColor}>{props.navMessage}</div>
-        <div className="scores">
-            Score: {props.score} <span className="pipe">|</span> High Score: {props.highScore}
-        </div>
+        <div className="title">Puppies 'n Kittens   Oh My!</div>
+        <div className="scores">Score: {props.score} <span className="pipe">|</span> High Score: {props.highScore}  </div>
     </div>
 );
 
